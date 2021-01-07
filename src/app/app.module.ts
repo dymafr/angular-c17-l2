82834b49a959872936d97f522220353d2bda5cb4
dymafr <erwan1309@hotmail.com>
2021-01-07 17:12:59 +0100

    2 files modified, 12 added, 12 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { CocktailListComponent } from "./cocktail-container/cocktail-list/cocktail-list.component";
-import { CocktailDetailsComponent } from "./cocktail-container/cocktail-details/cocktail-details.component";
-import { CocktailContainerComponent } from "./cocktail-container/cocktail-container.component";
 import { SelectedDirective } from "./shared/directives/selected.directive";
 import { PanierContainerComponent } from "./panier-container/panier-container.component";
 import { IngredientListComponent } from "./panier-container/ingredient-list/ingredient-list.component";
 import { RouterModule } from "@angular/router";
 import { APP_ROUTES } from "./app.routes";
 import { PanierService } from "./shared/services/panier.service";
-import { CocktailFormComponent } from "./cocktail-container/cocktail-form/cocktail-form.component";
 import { FilterPipe } from "./shared/pipes/filter.pipe";
 import { HttpClientModule } from "@angular/common/http";
 
