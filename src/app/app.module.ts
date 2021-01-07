68c7c757b5eb228dbe1ca5a3698c742af5d42e8b
dymafr <erwan1309@hotmail.com>
2021-01-07 17:14:21 +0100

    2 files modified, 6 added, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { SelectedDirective } from "./shared/directives/selected.directive";
-import { PanierContainerComponent } from "./panier-container/panier-container.component";
-import { IngredientListComponent } from "./panier-container/ingredient-list/ingredient-list.component";
 import { RouterModule } from "@angular/router";
 import { APP_ROUTES } from "./app.routes";
 import { PanierService } from "./shared/services/panier.service";
@@ -21,14 +19,7 @@ import { HttpClientModule } from "@angular/common/http";
     RouterModule.forRoot(APP_ROUTES),
     HttpClientModule
   ],
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SelectedDirective,
-    PanierContainerComponent,
-    IngredientListComponent,
-    FilterPipe
-  ],
+  declarations: [AppComponent, HeaderComponent, SelectedDirective, FilterPipe],
   bootstrap: [AppComponent],
   providers: [PanierService]
 })
