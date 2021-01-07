240e0c9ccee460cc6e15d06a4cd685d3420cc3d7
dymafr <erwan1309@hotmail.com>
2021-01-07 17:14:52 +0100

    1 file modified

--- src/app/features/panier/panier.module.ts
+++ src/app/features/panier/panier.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { PanierContainerComponent } from "./panier-container/panier-container.component";
+import { IngredientListComponent } from "./panier-container/ingredient-list/ingredient-list.component";
 
 @NgModule({
   declarations: [PanierContainerComponent, IngredientListComponent],
