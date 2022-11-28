112154546489cd8c6cd76750b018d7eb09292c8e
dymafr <32801166+dymafr@users.noreply.github.com>
2022-11-28 11:46:50 +0100

    1 file modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,21 @@
-import { Routes } from "@angular/router";
-import { PanierContainerComponent } from "./panier-container/panier-container.component";
-import { CocktailContainerComponent } from "./cocktail-container/cocktail-container.component";
-import { CocktailDetailsComponent } from "./cocktail-container/cocktail-details/cocktail-details.component";
-import { CocktailFormComponent } from "./cocktail-container/cocktail-form/cocktail-form.component";
+import { Routes } from '@angular/router';
+import { CocktailContainerComponent } from './features/cocktail/cocktail-container/cocktail-container.component';
+import { CocktailDetailsComponent } from './features/cocktail/cocktail-container/cocktail-details/cocktail-details.component';
+import { CocktailFormComponent } from './features/cocktail/cocktail-container/cocktail-form/cocktail-form.component';
+import { PanierContainerComponent } from './features/panier/panier-container/panier-container.component';
 
 export const APP_ROUTES: Routes = [
-  { path: "", redirectTo: "cocktails", pathMatch: "full" },
+  { path: '', redirectTo: 'cocktails', pathMatch: 'full' },
   {
-    path: "cocktails",
+    path: 'cocktails',
     component: CocktailContainerComponent,
     children: [
-      { path: "new", component: CocktailFormComponent },
-      { path: ":index/edit", component: CocktailFormComponent },
+      { path: 'new', component: CocktailFormComponent },
+      { path: ':index/edit', component: CocktailFormComponent },
 
-      { path: ":index", component: CocktailDetailsComponent },
-      { path: "", redirectTo: "0", pathMatch: "full" }
-    ]
+      { path: ':index', component: CocktailDetailsComponent },
+      { path: '', redirectTo: '0', pathMatch: 'full' },
+    ],
   },
-  { path: "panier", component: PanierContainerComponent }
+  { path: 'panier', component: PanierContainerComponent },
 ];
